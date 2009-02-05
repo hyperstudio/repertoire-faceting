783f16aea2c5b520f92f2bca43c9f29cd52eddc3
Christopher York <yorkc@mit.edu>
2009-02-05 14:27:52 +0500

    Simplified SQL API

--- ext/signature.sql
+++ ext/signature.sql
@@ -1,6 +1,31 @@
 --
--- Basic datatype support ('signature') for Repertoire faceting module.
+-- In-database support for Repertoire faceting module.
 --
+-- This library adds scalable faceted indexing to the PostgreSQL database.
+-- Basic approach is similar to other faceted browsers (Solr, Exhibit): an inverted bitmap index
+-- allows fast computation of facet value counts, given a base context and prior facet refinements.
+--
+-- Bitsets can also be used to compute the result set of items.
+--
+-- The library consists of: 
+--
+--   (a) a user defined bitset datatype ('signature') for storing inverted indices
+--   from facet values to items, and doing refinements and counts on items with a given
+--   facet value (see signature.sql, signature.c)
+--
+--   (b) facilities for adding a packed (continuous) id sequence to the main item table.  packed ids
+--   are used in the facet value signatures
+--
+--   (c) utility functions for creating/updating packed ids and facet value index tables, esp. for
+--   use with Repertoire crontab support
+--
+--
+-- Installation
+--
+--    (1) gem install repertoire-faceting [ builds c extensions automatically ]
+--    (2) psql -Upostgres <your database> -f ext/signature.sql [ installs SQL wrapper functions ]
+--    (3) [ optional ] install Repertoire crontab support [ see wiki ]
+--  
 
 SET search_path TO 'public';
 
@@ -76,23 +101,23 @@ CREATE OR REPLACE FUNCTION sig_xor( signature )
  AS 'signature.so', 'sig_xor'
  LANGUAGE C STRICT IMMUTABLE;
 
- -- aggregate functions for faceting
+-- aggregate functions for faceting
 
- CREATE AGGREGATE sig_collect( int )
- (
- 	sfunc = sig_set,
- 	stype = signature,
- 	initcond = ''
- );
+CREATE AGGREGATE signature( int )
+(
+	sfunc = sig_set,
+	stype = signature,
+	initcond = ''
+);
 
- CREATE AGGREGATE sig_collect( signature )
- (
- 	sfunc = sig_or,
- 	stype = signature
- );
+CREATE AGGREGATE collect( signature )
+(
+	sfunc = sig_or,
+	stype = signature
+);
 
- CREATE AGGREGATE sig_filter( signature )
- (
-     sfunc = sig_and,
-     stype = signature
- );
+CREATE AGGREGATE filter( signature )
+(
+   sfunc = sig_and,
+   stype = signature
+);
